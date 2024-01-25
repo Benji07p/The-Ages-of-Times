59fc0fc053fbeab351b049cfd31f9c4f09267780
Benji07p <Benji07p@gmail.com>
2024-01-25 18:18:37 +0100

    Add Craft for Block to ingot

--- kubejs/server_scripts/anvil.js
+++ kubejs/server_scripts/anvil.js
@@ -1,5 +1,5 @@
 onEvent('recipes', event => {
-    function anvil(itemin, itemout){
+    function anvil(itemin, itemout, count){
     event.custom(
         {
             "type": "lychee:anvil_crafting",
@@ -12,7 +12,8 @@ onEvent('recipes', event => {
                 }
             ],
             "item_out": {
-                "item": itemout
+                "item": itemout,
+                "count": count
             },
             "level_cost": 1,
             "material_cost": 1,
@@ -29,7 +30,7 @@ onEvent('recipes', event => {
         }
     )}
 
-    function anvilFall(itemin, itemout){
+    function anvilFall(itemin, itemout, count){
         event.custom(
             {
                 "type": "lychee:block_crushing",
@@ -42,32 +43,43 @@ onEvent('recipes', event => {
                     {
                         "type": "drop_item",
                         "item": itemout,
-                        "count": 1
+                        "count": count
                     }
                 ]
             }
         )}
 
-    anvil("minecraft:copper_ingot", "create:copper_sheet")
-    anvil("electrodynamics:ingottin", "chemlib:tin_plate")
-    anvil("electrodynamics:ingotbronze", "electrodynamics:platebronze")
-    anvil("electrodynamics:ingotsilver", "immersiveengineering:plate_silver")
-    anvil("minecraft:gold_ingot", "create:golden_sheet")
-    anvil("immersiveengineering:ingot_electrum", "immersiveengineering:plate_electrum")
-    anvil("electrodynamics:ingotlead", "electrodynamics:platelead")
-    anvil("minecraft:iron_ingot", "electrodynamics:plateiron")
-    anvil("create:brass_ingot", "create:brass_sheet")
-    anvil("immersiveengineering:ingot_nickel", "immersiveengineering:plate_nickel")
-    anvil("create:zinc_ingot", "createaddition:zinc_sheet")
-    anvilFall("minecraft:copper_ingot", "create:copper_sheet")
-    anvilFall("electrodynamics:ingottin", "chemlib:tin_plate")
-    anvilFall("electrodynamics:ingotbronze", "electrodynamics:platebronze")
-    anvilFall("electrodynamics:ingotsilver", "immersiveengineering:plate_silver")
-    anvilFall("minecraft:gold_ingot", "create:golden_sheet")
-    anvilFall("immersiveengineering:ingot_electrum", "immersiveengineering:plate_electrum")
-    anvilFall("electrodynamics:ingotlead", "electrodynamics:platelead")
-    anvilFall("minecraft:iron_ingot", "electrodynamics:plateiron")
-    anvilFall("create:brass_ingot", "create:brass_sheet")
-    anvilFall("immersiveengineering:ingot_nickel", "immersiveengineering:plate_nickel")
-    anvilFall("create:zinc_ingot", "createaddition:zinc_sheet")
+    anvil("minecraft:copper_ingot", "create:copper_sheet", 1)
+    anvil("electrodynamics:ingottin", "chemlib:tin_plate", 1)
+    anvil("electrodynamics:ingotbronze", "electrodynamics:platebronze", 1)
+    anvil("electrodynamics:ingotsilver", "immersiveengineering:plate_silver", 1)
+    anvil("minecraft:gold_ingot", "create:golden_sheet", 1)
+    anvil("immersiveengineering:ingot_electrum", "immersiveengineering:plate_electrum", 1)
+    anvil("electrodynamics:ingotlead", "electrodynamics:platelead", 1)
+    anvil("minecraft:iron_ingot", "electrodynamics:plateiron", 1)
+    anvil("create:brass_ingot", "create:brass_sheet", 1)
+    anvil("immersiveengineering:ingot_nickel", "immersiveengineering:plate_nickel", 1)
+    anvil("create:zinc_ingot", "createaddition:zinc_sheet", 1)
+    anvil("minecraft:copper_block", "minecraft:copper_ingot", 9)
+    anvil("electrodynamics:resourceblocktin", "electrodynamics:ingottin", 9)
+    anvil("electrodynamics:resourceblockbronze", "electrodynamics:ingotbronze", 9)
+    anvil("electrodynamics:resourceblocksilver", "electrodynamics:ingotsilver", 9)
+    anvil("minecraft:gold_block", "minecraft:gold_ingot", 9)
+    anvil("immersiveengineering:storage_electrum", "immersiveengineering:ingot_electrum", 9)
+    anvil("electrodynamics:resourceblocklead", "electrodynamics:ingotlead", 9)
+    anvil("minecraft:iron_block", "minecraft:iron_ingot", 9)
+    anvil("create:brass_block", "create:brass_ingot", 9)
+    anvil("immersiveengineering:storage_nickel", "immersiveengineering:ingot_nickel", 9)
+    anvil("create:zinc_block", "create:zinc_ingot", 9)
+    anvilFall("minecraft:copper_ingot", "create:copper_sheet", 1)
+    anvilFall("electrodynamics:ingottin", "chemlib:tin_plate", 1)
+    anvilFall("electrodynamics:ingotbronze", "electrodynamics:platebronze", 1)
+    anvilFall("electrodynamics:ingotsilver", "immersiveengineering:plate_silver", 1)
+    anvilFall("minecraft:gold_ingot", "create:golden_sheet", 1)
+    anvilFall("immersiveengineering:ingot_electrum", "immersiveengineering:plate_electrum", 1)
+    anvilFall("electrodynamics:ingotlead", "electrodynamics:platelead", 1)
+    anvilFall("minecraft:iron_ingot", "electrodynamics:plateiron", 1)
+    anvilFall("create:brass_ingot", "create:brass_sheet", 1)
+    anvilFall("immersiveengineering:ingot_nickel", "immersiveengineering:plate_nickel", 1)
+    anvilFall("create:zinc_ingot", "createaddition:zinc_sheet", 1)
 })
