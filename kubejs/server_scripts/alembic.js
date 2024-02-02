adf92b2520038a1cb122d1c6d03cc8a08ac3b2e6
Benji07p <Benji07p@gmail.com>
2024-02-02 19:33:25 +0100

    Add Recipes for Bismuth and Hydrogen

--- kubejs/server_scripts/alembic.js
+++ kubejs/server_scripts/alembic.js
@@ -1,5 +1,6 @@
 onEvent('recipes', event => {
 
+    function alembic(input, amount1, fluidinput, amount2, fluidoutput, amount3, output1, amount4, chance1, output2, amount5, chance2){
     event.custom(
         {
             "type": "custommachinery:custom_machine",
@@ -9,40 +10,47 @@ onEvent('recipes', event => {
                 {
                     "type": "custommachinery:item",
                     "mode": "input",
-                    "item": "chemlib:iron",
-                    "amount": 3,
+                    "item": input,
+                    "amount": amount1,
                     "slot": "input1"
                 },
                 {
                     "type": "custommachinery:fluid",
                     "mode": "input",
-                    "fluid": "minecraft:water",
-                    "amount": 100
+                    "fluid": fluidinput,
+                    "amount": amount2
                 },
                 {
                     "type": "custommachinery:fluid",
                     "mode": "output",
-                    "fluid": "chemlib:hydrogen_fluid",
-                    "amount": 200
+                    "fluid": fluidoutput,
+                    "amount": amount3
                 },
                 {
                     "type": "custommachinery:item",
                     "mode": "output",
-                    "item": "chemlib:iron_oxide",
-                    "amount": 1,
+                    "item": output1,
+                    "amount": amount4,
                     "slot": "input5",
-                    "chance": 0.5
+                    "chance": chance1
                 },
                 {
                     "type": "custommachinery:item",
                     "mode": "output",
-                    "item": "chemlib:iron_ii_oxide",
-                    "amount": 1,
+                    "item": output2,
+                    "amount": amount5,
                     "slot": "input6",
-                    "chance": 0.5
+                    "chance": chance2
                 }
 			]
 		}
-	)
+	)}
+
+    //Hydrogen
+    alembic("chemlib:iron", 1, "minecraft:water", 100, "chemlib:hydrogen_fluid", 100, "chemlib:iron_ii_oxide", 1, 1, "minecraft:air", 1, 0.01)
+    alembic("chemlib:iron", 2, "minecraft:water", 300, "chemlib:hydrogen_fluid", 300, "chemlib:iron_oxide", 1, 1, "minecraft:air", 1, 0.01)
+
+    //Bismuth
+    alembic("electrodynamics:raworelead", 10, "minecraft:water", 100, "tconstruct:molten_lead", 90, "chemlib:bismuth", 1, 0.5, "minecraft:air", 1, 0.01)
 
 })
