9f24cea622558c6951ba94067f3c3f8179a9262d
Benji07p <Benji07p@gmail.com>
2024-02-07 15:20:56 +0100

    Make Horse move in cercle during recipes

--- kubejs/server_scripts/horse.js
+++ kubejs/server_scripts/horse.js
@@ -4,7 +4,7 @@ onEvent('recipes', event => {
         {
             "type": "custommachinery:custom_machine",
             "machine": "taotmachinery:horse_power",
-            "time": 40000,
+            "time": 3,
             "requirements": [
                 {
                     "type": "custommachinery:entity",
@@ -18,23 +18,22 @@ onEvent('recipes', event => {
                 {
                     "type": "custommachinery:contraption",
                     "mode": "output",
-                    "speed": 32,
+                    "speed": 48,
                     "stress": 256
 				},
                 {
                     "type": "custommachinery:command",
-                    "command": "/execute as @e[type=horse,distance=..2] at @s if blocks 424 -63 -132 424 -63 -132 424 -63 -132 all run tp @s ~1 ~ ~",
+                    "command": "/execute as @e[type=minecraft:armor_stand, distance=..1] at @s run execute as @e[type=minecraft:horse,distance=..3] run tp @s ^2 ^ ^ facing ^270 ^ ^",
                     "permission": 2,
                     "phase": "crafting_tickable"
                 },
                 {
                     "type": "custommachinery:command",
-                    "command": "/execute as @e[type=horse,distance=..2] at @s if blocks 424 -63 -132 424 -63 -132 424 -63 -132 all run tp @s ~ ~ ~-2",
+                    "command": "/execute as @e[type=minecraft:armor_stand, distance=..1] at @s run tp @s ~ ~ ~ facing ^-0.01 ^ ^",
                     "permission": 2,
-                    "phase": "starting"
+                    "phase": "crafting_tickable"
                 }
 			]
 		}
 	)
-
 })
