dbd62ca88b1cfe6191a951c4c0d227c90a6f549b
Benji07p <Benji07p@gmail.com>
2024-02-09 18:51:16 +0100

    Add Recipes for Stone Tablet and Writing on Stone Tablet

---
+++ kubejs/server_scripts/writting.js
@@ -0,0 +1,33 @@
+onEvent('recipes', event => {
+    
+    function writing(output, input){
+    event.shaped(output, [
+		'A'
+	], {
+	A: input
+	}).replaceIngredient('multibeds:custom', 'the_ages_of_times:stone_tablet')
+    }
+
+    writing('the_ages_of_times:aletter', Item.of('multibeds:custom', '{BlockEntityTag:{blanket_item:{Count:1b,id:"multibeds:blanket_light_gray",tag:{artwork:"custom",compressed:1b,indices:[B;120B,-38B,-5B,-13B,-121B,16B,96B,64B,2B,-28B,-16B,105B,109B,62B,101B,124B,0B,44B,13B,110B,65B],palette:0b,pattern:"grid"}},mirror:0b,pillow_item:{Count:1b,id:"multibeds:pillow",tag:{rgb16:2457s,rgbsimple:8b}},sheet_item:{Count:1b,id:"multibeds:sheet",tag:{rgb16:2457s,rgbsimple:8b}},texture_item:{Count:1b,id:"minecraft:stone"}}}'))
+    writing('the_ages_of_times:bletter', Item.of('multibeds:custom', '{BlockEntityTag:{blanket_item:{Count:1b,id:"multibeds:blanket_light_gray",tag:{artwork:"custom",compressed:1b,indices:[B;120B,-38B,-5B,-13B,7B,25B,48B,48B,-4B,-63B,-32B,51B,32B,1B,-120B,24B,101B,-4B,-63B,101B,31B,0B,-100B,75B,122B,17B],palette:0b,pattern:"grid"}},mirror:0b,pillow_item:{Count:1b,id:"multibeds:pillow",tag:{rgb16:2457s,rgbsimple:8b}},sheet_item:{Count:1b,id:"multibeds:sheet",tag:{rgb16:2457s,rgbsimple:8b}},texture_item:{Count:1b,id:"minecraft:stone"}}}'))
+    writing('the_ages_of_times:cletter', Item.of('multibeds:custom', '{BlockEntityTag:{blanket_item:{Count:1b,id:"multibeds:blanket_light_gray",tag:{artwork:"custom",compressed:1b,indices:[B;120B,-38B,-5B,-13B,-121B,16B,96B,64B,1B,67B,-113B,-113B,31B,0B,0B,36B,68B,86B,-95B],palette:0b,pattern:"grid"}},mirror:0b,pillow_item:{Count:1b,id:"multibeds:pillow",tag:{rgb16:2457s,rgbsimple:8b}},sheet_item:{Count:1b,id:"multibeds:sheet",tag:{rgb16:2457s,rgbsimple:8b}},texture_item:{Count:1b,id:"minecraft:stone"}}}'))
+    writing('the_ages_of_times:dletter', Item.of('multibeds:custom', '{BlockEntityTag:{blanket_item:{Count:1b,id:"multibeds:blanket_light_gray",tag:{artwork:"custom",compressed:1b,indices:[B;120B,-38B,-5B,-13B,7B,25B,48B,48B,-4B,-63B,-32B,51B,32B,1B,-120B,-40B,-48B,-30B,-29B,-9B,31B,0B,-98B,77B,102B,97B],palette:0b,pattern:"grid"}},mirror:0b,pillow_item:{Count:1b,id:"multibeds:pillow",tag:{rgb16:2457s,rgbsimple:8b}},sheet_item:{Count:1b,id:"multibeds:sheet",tag:{rgb16:2457s,rgbsimple:8b}},texture_item:{Count:1b,id:"minecraft:stone"}}}'))
+    writing('the_ages_of_times:eletter', Item.of('multibeds:custom', '{BlockEntityTag:{blanket_item:{Count:1b,id:"multibeds:blanket_light_gray",tag:{artwork:"custom",compressed:1b,indices:[B;120B,-38B,-5B,-13B,-121B,16B,96B,64B,1B,-108B,-13B,81B,13B,-58B,-28B,-45B,-52B,62B,-84B,0B,0B,36B,59B,104B,89B],palette:0b,pattern:"grid"}},mirror:0b,pillow_item:{Count:1b,id:"multibeds:pillow",tag:{rgb16:2457s,rgbsimple:8b}},sheet_item:{Count:1b,id:"multibeds:sheet",tag:{rgb16:2457s,rgbsimple:8b}},texture_item:{Count:1b,id:"minecraft:stone"}}}'))
+    writing('the_ages_of_times:fletter', Item.of('multibeds:custom', '{BlockEntityTag:{blanket_item:{Count:1b,id:"multibeds:blanket_light_gray",tag:{artwork:"custom",compressed:1b,indices:[B;120B,-38B,-5B,-13B,-121B,16B,96B,64B,1B,-108B,-13B,81B,-51B,-59B,-28B,83B,-37B,62B,-4B,124B,0B,-128B,-58B,74B,-47B],palette:0b,pattern:"grid"}},mirror:0b,pillow_item:{Count:1b,id:"multibeds:pillow",tag:{rgb16:2457s,rgbsimple:8b}},sheet_item:{Count:1b,id:"multibeds:sheet",tag:{rgb16:2457s,rgbsimple:8b}},texture_item:{Count:1b,id:"minecraft:stone"}}}'))
+    writing('the_ages_of_times:gletter', Item.of('multibeds:custom', '{BlockEntityTag:{blanket_item:{Count:1b,id:"multibeds:blanket_light_gray",tag:{artwork:"custom",compressed:1b,indices:[B;120B,-38B,-5B,-13B,-121B,16B,96B,64B,1B,84B,-30B,-93B,-101B,-113B,-61B,62B,106B,-16B,-15B,3B,0B,80B,-123B,114B,49B],palette:0b,pattern:"grid"}},mirror:0b,pillow_item:{Count:1b,id:"multibeds:pillow",tag:{rgb16:2457s,rgbsimple:8b}},sheet_item:{Count:1b,id:"multibeds:sheet",tag:{rgb16:2457s,rgbsimple:8b}},texture_item:{Count:1b,id:"minecraft:stone"}}}'))
+    writing('the_ages_of_times:hletter', Item.of('multibeds:custom', '{BlockEntityTag:{blanket_item:{Count:1b,id:"multibeds:blanket_light_gray",tag:{artwork:"custom",compressed:1b,indices:[B;120B,-38B,-5B,-13B,-25B,-49B,31B,6B,40B,-8B,3B,5B,-44B,-26B,83B,10B,104B,-23B,62B,0B,20B,97B,-99B,-127B],palette:0b,pattern:"grid"}},mirror:0b,pillow_item:{Count:1b,id:"multibeds:pillow",tag:{rgb16:2457s,rgbsimple:8b}},sheet_item:{Count:1b,id:"multibeds:sheet",tag:{rgb16:2457s,rgbsimple:8b}},texture_item:{Count:1b,id:"minecraft:stone"}}}'))
+
+    event.shaped(Item.of('multibeds:custom', '{BlockEntityTag:{blanket_item:{Count:1b,id:"multibeds:blanket_light_gray",tag:{pattern:"grid"}},mirror:0b,pillow_item:{Count:1b,id:"multibeds:pillow",tag:{rgb16:2457s,rgbsimple:8b}},sheet_item:{Count:1b,id:"multibeds:sheet",tag:{rgb16:2457s,rgbsimple:8b}},texture_item:{Count:1b,id:"minecraft:stone"}}}'), [
+		'S'
+	], {
+	S: 'the_ages_of_times:stone_tablet'
+	})
+
+    event.shaped(Item.of('multibeds:custom', '{BlockEntityTag:{blanket_item:{Count:1b,id:"multibeds:blanket_light_gray",tag:{pattern:"grid"}},mirror:0b,pillow_item:{Count:1b,id:"multibeds:pillow",tag:{rgb16:2457s,rgbsimple:8b}},sheet_item:{Count:1b,id:"multibeds:sheet",tag:{rgb16:2457s,rgbsimple:8b}},texture_item:{Count:1b,id:"minecraft:stone"}}}'), [
+		' S ',
+        'SSS',
+        'SSS'
+	], {
+	S: 'minecraft:stone'
+	})
+})
