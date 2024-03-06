79f94865862db370881a7c8836afdd092d82acc3
Benji07p <Benji07p@gmail.com>
2024-03-06 17:49:49 +0100

    Add Recipes for Phosphorus

--- kubejs/server_scripts/alembic.js
+++ kubejs/server_scripts/alembic.js
@@ -107,7 +107,7 @@ onEvent('recipes', event => {
                 ]
             }
         )}
-        function oxydoreduction(input, amount1, input45, amount45, input9, amount9, fluidinput, amount2, fluidoutput, amount3, output1, amount4, chance1, output2, amount5, chance2){
+        function oxydoreduction(input, amount1, input45, amount45, input9, amount9, fluidinput, amount2, chance3, fluidoutput, amount3, output1, amount4, chance1, output2, amount5, chance2){
             event.custom(
                 {
                     "type": "custommachinery:custom_machine",
@@ -139,7 +139,8 @@ onEvent('recipes', event => {
                             "type": "custommachinery:fluid",
                             "mode": "input",
                             "fluid": fluidinput,
-                            "amount": amount2
+                            "amount": amount2,
+                            "chance": chance3
                         },
                         {
                             "type": "custommachinery:fluid",
@@ -272,13 +273,20 @@ onEvent('recipes', event => {
             }
         )}
 
-        function melter2(input9, amount9, output1, amount4, chance1, output2, amount5, chance2){
+        function melter2(input45, amount45, input9, amount9, output1, amount4, chance1, output2, amount5, chance2){
             event.custom(
                 {
                     "type": "custommachinery:custom_machine",
                     "machine": "taotmachinery:alembic",
                     "time": 80,
                     "requirements": [
+                        {
+                            "type": "custommachinery:item",
+                            "mode": "input",
+                            "item": input45,
+                            "amount": amount45,
+                            "slot": "input1"
+                        },
                         {
                             "type": "custommachinery:item",
                             "mode": "input",
@@ -476,7 +484,11 @@ function melting2(input, amount1, fluidoutput, amount3, output1, amount4, chance
     //Phosphorus
     fill("the_ages_of_times:beaker", "the_ages_of_times:iron_sulfate", 63, "chemlib:iron_ii_sulfate")
     unfill("chemlib:iron_ii_sulfate", "the_ages_of_times:iron_sulfate", 63, "the_ages_of_times:beaker")
-    melter2("chemlib:iron_ii_sulfate", 8, "chemlib:iron_ii_sulfate_dust", 1, 1, "the_ages_of_times:beaker", 1, 1)
+    melter2("the_ages_of_times:watch_glass", 1, "chemlib:iron_ii_sulfate", 8, "chemlib:iron_ii_sulfate_dust", 1, 1, "the_ages_of_times:beaker", 8, 1)
+    fill("the_ages_of_times:beaker", "the_ages_of_times:golden_water", 63, "the_ages_of_times:gold_water")
+    unfill("the_ages_of_times:gold_water", "the_ages_of_times:golden_water", 63, "the_ages_of_times:beaker")
+    distillation("chemlib:iron_ii_sulfate_dust", 1, "create:filter", 1, "the_ages_of_times:golden_water", 100, "chemlib:sulfuric_acid_fluid", 100, "the_ages_of_times:vivianite_dust", 1, 1, "create:filter", 1, 1)
+    oxydoreduction("the_ages_of_times:test_tube", 3, "the_ages_of_times:beaker", 2, "the_ages_of_times:vivianite_dust", 1, "chemlib:sulfuric_acid_fluid", 200, 0.0, "minecraft:water", 200, "chemlib:phosphoric_acid", 2, 1, "chemlib:iron", 3, 1)
 
     //Bismuth
     meltingdistillation("the_ages_of_times:test_tube", 1, "electrodynamics:raworelead", 10, "minecraft:water", 100, "tconstruct:molten_lead", 900, "chemlib:bismuth", 1, 0.5, "minecraft:air", 1, 0.01)
@@ -488,11 +500,11 @@ function melting2(input, amount1, fluidoutput, amount3, output1, amount4, chance
     fill("the_ages_of_times:balloon", "chemlib:carbon_dioxide_fluid", 63, "chemlib:carbon_dioxide")
 
     //Chlorine
-    melter2("electrodynamics:crystalhalite", 1, "chemlib:ammonium_chloride_dust", 1, 0.1, "minecraft:air", 1, 1)
+    melter2("the_ages_of_times:watch_glass", 1, "electrodynamics:crystalhalite", 1, "chemlib:ammonium_chloride_dust", 1, 0.1, "minecraft:air", 1, 1)
     melter("the_ages_of_times:erlenmeyer", 8, "the_ages_of_times:balloon", 8, "chemlib:ammonium_chloride_dust", 1, "chemlib:hydrochloric_acid", 8, 1, "chemlib:ammonia", 8, 1)
     unfill("chemlib:ammonia", "chemlib:ammonia_fluid", 63, "the_ages_of_times:balloon")
     unfill("chemlib:hydrochloric_acid", "chemlib:hydrochloric_acid_fluid", 63, "the_ages_of_times:erlenmeyer")
-    oxydoreduction("the_ages_of_times:beaker", 1, "the_ages_of_times:gas_test_tube", 2, "chemlib:manganese_oxide_dust", 1, "chemlib:hydrochloric_acid_fluid", 250, "minecraft:water", 125, "the_ages_of_times:manganese_chloride_ii", 1, 1, "chemlib:chlorine", 2, 1)
+    oxydoreduction("the_ages_of_times:beaker", 1, "the_ages_of_times:gas_test_tube", 2, "chemlib:manganese_oxide_dust", 1, "chemlib:hydrochloric_acid_fluid", 250, 1.0, "minecraft:water", 125, "the_ages_of_times:manganese_chloride_ii", 1, 1, "chemlib:chlorine", 2, 1)
 
     //Oxygen and Nitrogen
     meltingfluid("the_ages_of_times:beaker", 6, "chemlib:mercury_fluid", 100, "chemlib:nitrogen_fluid", 282, "the_ages_of_times:mercury_oxide", 3, 1, "the_ages_of_times:beaker", 3, 1)
