41f54a31bf16ef6184ce0130e1b247b21eca7205
Benji07p <Benji07p@gmail.com>
2024-08-06 19:46:28 +0200

    Add JEI integration for Wandering Trader

--- kubejs/server_scripts/wandering_trader.js
+++ kubejs/server_scripts/wandering_trader.js
@@ -1,3 +1,42 @@
 onEvent("morejs.wanderer.trades", (event) => {
     const trade = event.addTrade(1, ["the_ages_of_times:market"], "farmingforblockheads:market");
 })
+
+
+onEvent('recipes', event => {
+
+    function wandering(inb, count, out){
+    event.custom(
+        {
+            "type": "custommachinery:custom_machine",
+            "machine": "taotmachinery:wandering_trader",
+            "time": 0,
+            "requirements": [
+                {
+                    "type": "custommachinery:item",
+                    "mode": "input",
+                    "item": inb,
+                    "amount": count,
+                    "slot": "input1"
+                },
+                {
+                    "type": "custommachinery:item",
+                    "mode": "input",
+                    "item": "the_ages_of_times:wandering_traders_block",
+                    "amount": 1,
+                    "slot": "input2"
+                },
+                {
+                    "type": "custommachinery:item",
+                    "mode": "output",
+                    "item": out,
+                    "amount": 1,
+                    "slot": "input3"
+                }
+			]
+		}
+	)}
+
+    wandering("the_ages_of_times:market", 1, "farmingforblockheads:market")
+
+})
