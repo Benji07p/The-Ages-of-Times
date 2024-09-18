e531f56be2318ff0061b3bd74f79510975488bea
Benji07p <Benji07p@gmail.com>
2024-09-18 21:13:39 +0200

    Fix Quantum Intriquer

--- kubejs/server_scripts/quantumintriquer.js
+++ kubejs/server_scripts/quantumintriquer.js
@@ -26,13 +26,31 @@ onEvent('recipes', event => {
                     "action": "replace_destroy",
                     "pos": [0, 0, -1, 0, 0, -1],
                     "block": "minecraft:redstone_wire[power=15]"
-                },
+                }
+			]
+		}
+	)
+
+    event.custom(
+        {
+            "type": "custommachinery:custom_machine",
+            "machine": "taotmachinery:quantum_bedrock",
+            "time": 1,
+            "requirements": [
                 {
                     "type": "custommachinery:block",
                     "mode": "input",
+                    "action": "check",
+                    "pos": [0, -1, 0, 0, -1, 0],
+                    "filter": ["minecraft:redstone_wire[power=15]"],
+                    "whitelist": true
+                },
+                {
+                    "type": "custommachinery:block",
+                    "mode": "output",
                     "action": "replace_destroy",
-                    "pos": [0, 0, -1, 0, 0, -1],
-                    "block": "minecraft:redstone_wire[power=0]"
+                    "pos": [0, -1, 0, 0, -1, 0],
+                    "block": "minecraft:redstone_wire"
                 }
 			]
 		}
