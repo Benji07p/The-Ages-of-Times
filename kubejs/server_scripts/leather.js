a7a99cb3c0581855207c5867d2b284f792a2b11c
Benji07p <Benji07p@gmail.com>
2024-09-28 15:37:02 +0200

    Add Recipe to Automate Leather

--- kubejs/server_scripts/leather.js
+++ kubejs/server_scripts/leather.js
@@ -36,6 +36,83 @@ onEvent('recipes', event => {
         }
     )}
 
+    function rackauto(int, out){
+        event.custom(
+            {
+              "type": "create:sequenced_assembly",
+              "ingredient": {
+                "item": out
+              },
+              "transitionalItem": {
+                "item": int
+              },
+              "sequence": [
+                {
+                  "type": "create:filling",
+                  "ingredients": [
+                    {
+                      "item": int
+                    },
+                    {
+                      "fluid": "the_ages_of_times:creative_fluid",
+                      "amount": 250
+                    }
+                  ],
+                  "results": [
+                    {
+                      "item": int
+                    }
+                  ]
+                },
+                {
+                    "type": "create:cutting",
+                    "ingredients": [
+                        {
+                            "item": int
+                        }
+                    ],
+                    "results": [
+                        {
+                            "item": int
+                        }
+				    ],
+				    "processingTime": 50
+                }
+			  ],
+              "results": [
+                {
+                  "item": out,
+                  "chance": 1.0
+                }
+              ],
+              "loops": 1
+            }
+        )
+    }
+
+    function rackauto2(int, out, nbt){
+        event.custom(
+            {
+                "type": "create:emptying",
+                "ingredients": [
+                  {
+                    "item": int
+                  }
+                ],
+                "results": [
+                  {
+                    "item": out,
+                    "nbt": nbt
+                  },
+                  {
+                    "fluid": "minecraft:water",
+                    "amount": 200
+                  }
+                ]
+              }
+        )
+    }
+
     function spout(int, fluid, amount, out){
         event.custom(
             {
@@ -280,6 +357,15 @@ onEvent('recipes', event => {
     RacktoItem("the_ages_of_times:hide_dried", "the_ages_of_times:racksalted")
     RacktoItem("the_ages_of_times:hide_raw", "the_ages_of_times:rackraw")
     RacktoItem("atum:scroll", "the_ages_of_times:white_leather_rack")
+
+    rackauto("the_ages_of_times:hide_salted", "the_ages_of_times:hide_dried")
+    rackauto("the_ages_of_times:hide_clean", "the_ages_of_times:hide_raw")
+    rackauto("the_ages_of_times:hide_tanned", "minecraft:leather")
+    rackauto("the_ages_of_times:white_leather", "atum:scroll")
+    rackauto2("the_ages_of_times:hide_salted", "the_ages_of_times:hide_salted", '{SequencedAssembly:{Progress:0.5f,Step:1,id:"create:kjs_bgs4ekcopkf60rjfbc3wf0obl"}}')
+    rackauto2("the_ages_of_times:hide_clean", "the_ages_of_times:hide_clean", '{SequencedAssembly:{Progress:0.5f,Step:1,id:"create:kjs_chnzfhf96hnlme2nms6cuemyx"}}')
+    rackauto2("the_ages_of_times:hide_tanned", "the_ages_of_times:hide_tanned", '{SequencedAssembly:{Progress:0.5f,Step:1,id:"create:kjs_3z7yh45gm4qxq8oak6cfpxwd8"}}')
+    rackauto2("the_ages_of_times:white_leather", "the_ages_of_times:white_leather", '{SequencedAssembly:{Progress:0.5f,Step:1,id:"create:kjs_3k96dfi80d1l18nwqrh2pvgeq"}}')
 
     Chaudron("the_ages_of_times:hide_raw", "electrodynamics:dustsalt", 3, "the_ages_of_times:hide_salted", 2)
     Chaudron("the_ages_of_times:hide_raw", "electrodynamics:dustsalt", 2, "the_ages_of_times:hide_salted", 1)
