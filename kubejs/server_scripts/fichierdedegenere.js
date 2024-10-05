146e4bb74b5c11ef5cb2716be2ad309330b84569
Benji07p <Benji07p@gmail.com>
2024-10-05 16:48:04 +0200

    Add Skeleton and Wither Skeleton

---
+++ kubejs/server_scripts/fichierdedegenere.js
@@ -0,0 +1,44 @@
+onEvent('entity.hurt', event =>{
+    if (event.entity.type == 'minecraft:skeleton'){
+        
+        if (event.entity.block.id == "minecraft:wither_rose") {
+            event.entity.remove()
+            event.server.runCommandSilent(`execute in ${event.entity.level.dimension} run summon minecraft:wither_skeleton ${event.entity.x} ${event.entity.y} ${event.entity.z}`)
+            event.entity.block.set("minecraft:air")
+        }
+    }
+})
+
+onEvent('recipes', event => {
+    //Skeleton
+    function skeletton(int){
+	event.custom(
+		{
+		  "type": "lychee:block_interacting",
+		  "item_in": {
+			"item": "quark:soul_bead"
+		  },
+		  "block_in": int,
+		  "post": [
+			{
+			  "type": "execute",
+			  "command": "summon minecraft:skeleton ~ ~ ~",
+			  "hide": true
+			},
+			{
+				"type": "place",
+				"block": {
+					"blocks": ["air"]
+				}
+			}
+		  ]
+		}
+	)}
+
+    skeletton("davebuildingmod:skeleton_1")
+    skeletton("davebuildingmod:skeleton_2")
+    skeletton("davebuildingmod:skeleton_3")
+    skeletton("davebuildingmod:skeleton_4")
+    skeletton("davebuildingmod:skeleton_5")
+    skeletton("davebuildingmod:skeleton_6")
+})
