bb39be3c54922be6ad79ad336d860588f0df8305
Benji07p <Benji07p@gmail.com>
2025-09-02 17:01:27 +0200

    Fix of Gaz Recipes

--- kubejs/server_scripts/telescope.js
+++ kubejs/server_scripts/telescope.js
@@ -1,6 +1,6 @@
 onEvent('recipes', event => {
 
-    function spectroscopie(item3, chance, dim, time){
+    function spectroscopie(item1, chance1, item2, item3, chance, dim, time){
     event.custom(
         {
             "type": "custommachinery:custom_machine",
@@ -10,9 +10,17 @@ onEvent('recipes', event => {
                 {
                     "type": "custommachinery:item",
                     "mode": "input",
-                    "item": "quark:clear_shard",
+                    "item": item1,
                     "amount": 1,
                     "slot": "input1",
+                    "chance": chance1
+                },
+                {
+                    "type": "custommachinery:item",
+                    "mode": "input",
+                    "item": item2,
+                    "amount": 1,
+                    "slot": "input2",
                     "chance": 0
                 },
                 {
@@ -39,7 +47,7 @@ onEvent('recipes', event => {
 		}
 	)}
 
-    function spectroscopie2(item2, item3, chance){
+    function spectroscopie2(item1, chance1, item2, item3, chance){
         event.custom(
             {
                 "type": "custommachinery:custom_machine",
@@ -49,10 +57,10 @@ onEvent('recipes', event => {
                     {
                         "type": "custommachinery:item",
                         "mode": "input",
-                        "item": "quark:clear_shard",
+                        "item": item1,
                         "amount": 1,
                         "slot": "input1",
-                        "chance": 0
+                        "chance": chance1
                     },
                     {
                         "type": "custommachinery:item",
@@ -75,7 +83,7 @@ onEvent('recipes', event => {
         )
     }
 
-    function telescope1(item2, item3, dim){
+    function spacetelescope(item1, chance1, item2, item3, dim){
         event.custom(
             {
                 "type": "custommachinery:custom_machine",
@@ -85,9 +93,18 @@ onEvent('recipes', event => {
                     {
                         "type": "custommachinery:item",
                         "mode": "input",
+                        "item": item1,
+                        "amount": 1,
+                        "slot": "input1",
+                        "chance": chance1
+                    },
+                    {
+                        "type": "custommachinery:item",
+                        "mode": "input",
                         "item": item2,
                         "amount": 1,
-                        "slot": "input2"
+                        "slot": "input2",
+                        "chance": 0
                     },
                     {
                         "type": "custommachinery:item",
@@ -105,7 +122,7 @@ onEvent('recipes', event => {
         )
     }
 
-    function spacetelescope(item3, dim){
+    function telescope(item1, chance1, item2, item3, dim, time){
         event.custom(
             {
                 "type": "custommachinery:custom_machine",
@@ -114,27 +131,12 @@ onEvent('recipes', event => {
                 "requirements": [
                     {
                         "type": "custommachinery:item",
-                        "mode": "output",
-                        "item": item3,
-                        "slot": "output",
-                        "amount": 1
+                        "mode": "input",
+                        "item": item1,
+                        "amount": 1,
+                        "slot": "input1",
+                        "chance": chance1
                     },
-                    {
-                        "type": "custommachinery:dimension",
-                        "filter": dim
-                    }
-                ]
-            }
-        )
-    }
-
-    function telescope(item2, item3, dim, time){
-        event.custom(
-            {
-                "type": "custommachinery:custom_machine",
-                "machine": "taotmachinery:telescope",
-                "time": 100,
-                "requirements": [
                     {
                         "type": "custommachinery:item",
                         "mode": "input",
@@ -166,27 +168,27 @@ onEvent('recipes', event => {
         )
     }
 
-    spectroscopie("chemlib:helium", 0.01, "minecraft:overworld", "(6000, 12000)")
-    spectroscopie("chemlib:neon", 0.01, "minecraft:overworld", "(6000, 12000)")
-    spectroscopie("chemlib:argon", 0.01, "minecraft:overworld", "(6000, 12000)")
-    spectroscopie("chemlib:krypton", 0.01, "minecraft:overworld", "(6000, 12000)")
-    spectroscopie("chemlib:xenon", 0.01, "minecraft:overworld", "(6000, 12000)")
+    spectroscopie("the_ages_of_times:gas_test_tube", 0.01, "minecraft:glass_pane", "chemlib:helium", 0.01, "minecraft:overworld", "(6000, 12000)")
+    spectroscopie("the_ages_of_times:gas_test_tube", 0.01, "minecraft:glass_pane", "chemlib:neon", 0.01, "minecraft:overworld", "(6000, 12000)")
+    spectroscopie("the_ages_of_times:gas_test_tube", 0.01, "minecraft:glass_pane", "chemlib:argon", 0.01, "minecraft:overworld", "(6000, 12000)")
+    spectroscopie("the_ages_of_times:gas_test_tube", 0.01, "minecraft:glass_pane", "chemlib:krypton", 0.01, "minecraft:overworld", "(6000, 12000)")
+    spectroscopie("the_ages_of_times:gas_test_tube", 0.01, "minecraft:glass_pane", "chemlib:xenon", 0.01, "minecraft:overworld", "(6000, 12000)")
 
-    spectroscopie2("minecraft:water_bucket", "chemlib:cesium", 0.01)
-    spectroscopie2("electrodynamics:raworelepidolite", "chemlib:rubidium", 0.01)
-    spectroscopie2("create:zinc_ore", "chemlib:indium", 0.01)
-    spectroscopie2("minecraft:copper_ore", "chemlib:indium", 0.01)
-    spectroscopie2("create:raw_zinc", "chemlib:gallium", 0.01)
-    spectroscopie2("chemlib:tellurium_dust", "chemlib:thallium", 0.01)
-    spectroscopie2("chemlib:erbium_dust", "chemlib:holmium", 0.01)
-    spectroscopie2("chemlib:erbium_dust", "chemlib:thulium", 0.01)
-    spectroscopie2("the_ages_of_times:didymium", "chemlib:samarium", 0.01)
-    spectroscopie2("the_ages_of_times:ore_gadolinite", "chemlib:gadolinium", 0.01)
-    spectroscopie2("the_ages_of_times:ore_gadolinite", "chemlib:scandium", 0.01)
-    spectroscopie2("nuclearscience:polonium210", "chemlib:radium", 0.01)
-    spectroscopie2("chemlib:samarium_dust", "chemlib:europium", 0.01)
+    spectroscopie2("the_ages_of_times:test_tube", 0.01, "minecraft:water_bucket", "chemlib:cesium", 0.01)
+    spectroscopie2("the_ages_of_times:test_tube", 0.01, "electrodynamics:raworelepidolite", "chemlib:rubidium", 0.01)
+    spectroscopie2("the_ages_of_times:test_tube", 0.01, "create:zinc_ore", "chemlib:indium", 0.01)
+    spectroscopie2("the_ages_of_times:test_tube", 0.01, "minecraft:copper_ore", "chemlib:indium", 0.01)
+    spectroscopie2("the_ages_of_times:test_tube", 0.01, "create:raw_zinc", "chemlib:gallium", 0.01)
+    spectroscopie2("the_ages_of_times:test_tube", 0.01, "chemlib:tellurium_dust", "chemlib:thallium", 0.01)
+    // spectroscopie2("chemlib:erbium_dust", "chemlib:holmium", 0.01)
+    // spectroscopie2("chemlib:erbium_dust", "chemlib:thulium", 0.01)
+    spectroscopie2("the_ages_of_times:test_tube", 0.01, "the_ages_of_times:didymium", "chemlib:samarium", 0.01)
+    // spectroscopie2("the_ages_of_times:ore_gadolinite", "chemlib:gadolinium", 0.01)
+    // spectroscopie2("the_ages_of_times:ore_gadolinite", "chemlib:scandium", 0.01)
+    spectroscopie2("the_ages_of_times:test_tube", 0.01, "nuclearscience:polonium210", "chemlib:radium", 0.01)
+    spectroscopie2("the_ages_of_times:test_tube", 0.01, "chemlib:samarium_dust", "chemlib:europium", 0.01)
     
-    spacetelescope("the_ages_of_times:moon_coordinates", "beyond_earth:earth_orbit")
+    spacetelescope("minecraft:glass_pane", 0, "minecraft:glass_pane", "the_ages_of_times:moon_coordinates", "beyond_earth:earth_orbit")
 
-    telescope("the_ages_of_times:star_parchment", "the_ages_of_times:sky_parchment", "minecraft:overworld", "(12000,)")
+    telescope("minecraft:glass_pane", 0, "the_ages_of_times:star_parchment", "the_ages_of_times:sky_parchment", "minecraft:overworld", "(12000,)")
 })
